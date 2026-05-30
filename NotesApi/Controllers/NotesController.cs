0d0ecaff8f6682112de5f64715b0ceba0a5c8cde
Marcin Lis <pzx122026@student.poznan.merito.pl>
2026-05-30 12:30:50 +0200

    Dodaj pliki projektów.

---
+++ NotesApi/Controllers/NotesController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NotesApi.Data;
+using NotesApi.Models;
+using System.Security.Claims;
+
+namespace NotesApi.Controllers
+{
+    [ApiController]
+    [Route("/notes")]
+    [Authorize]
+    public class NotesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public NotesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        private int GetUserId()
+        {
+            return int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var userId = GetUserId();
+            var notes = _context.Notes.Where(n => n.UserId == userId).ToList();
+            return Ok(notes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var userId = GetUserId();
+            var note = _context.Notes.FirstOrDefault(n => n.Id == id && n.UserId == userId);
+
+            if (note == null) return NotFound();
+
+            return Ok(note);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Note newNote)
+        {
+            var userId = GetUserId();
+
+            newNote.UserId = userId;
+
+            _context.Notes.Add(newNote);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetById), new { id = newNote.Id }, newNote);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Note updatedNote)
+        {
+            var userId = GetUserId();
+
+            var note = _context.Notes.FirstOrDefault(n => n.Id == id);
+
+            if (note == null) return NotFound();
+
+            if (note.UserId != userId) return StatusCode(403);
+
+            note.Content = updatedNote.Content;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var userId = GetUserId();
+
+            var note = _context.Notes.FirstOrDefault(n => n.Id == id);
+
+            if (note == null) return NotFound();
+
+            if (note.UserId != userId) return StatusCode(403);
+
+            _context.Notes.Remove(note);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ NotesApi/Migrations/20260530101137_PierwszaBaza.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotesApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class PierwszaBaza : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
